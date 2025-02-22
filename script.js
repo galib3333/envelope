70699268bf713c4ff62828627ebea2b7958bd26d
Asadullah Al Galib <mohapurush6@gmail.com>
2025-02-22 21:19:52 +0600

    Initial Push

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const letterContext = document.querySelector(".letter-context");
+  letterContext.textContent = "Happy Birthday, Nafiza! Wishing you a day filled with love and joy!!! 🎉";
+});
